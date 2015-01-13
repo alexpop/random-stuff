fce046df82b9c8834102b98732b3314f24eb660c
Alex Pop <al3xpop@gmail.com>
2015-01-13 13:27:43 +0000

    add branch after path

--- custom_prompt_w_git.sh
+++ custom_prompt_w_git.sh
@@ -92,14 +92,14 @@ Jobs="\j"
 
 export PS1=$IBlack"["$Time12h$Color_Off'$(git branch &>/dev/null;\
 if [ $? -eq 0 ]; then \
-  echo "$(echo `git status` | grep "nothing to commit" > /dev/null 2>&1; \
+	echo " '$BYellow$PathShort'$(echo `git status` | grep "nothing to commit" > /dev/null 2>&1; \
   if [ "$?" -eq "0" ]; then \
     # @4 - Clean repository - nothing to commit
     echo "'$Green'"$(__git_ps1 " (%s)"); \
   else \
     # @5 - Changes to working tree
     echo "'$IRed'"$(__git_ps1 " {%s}"); \
-  fi) '$BYellow$PathShort$IBlack]\$$Color_Off' "; \
+  fi)'$IBlack]\$$Color_Off' "; \
 else \
   # @2 - Prompt when not in GIT repo
   echo " '$Yellow$PathShort$IBlack]\$$Color_Off' "; \
